21b5df2e937836e041bf4d34e1974a8fe484b31b
Ale Kiut <ale_kiut47@hotmail.com>
2019-10-28 22:02:42 -0600

    04 javascript

---
+++ 04_Javascript/main.js
@@ -0,0 +1,50 @@
+let numero = 10
+let texto = "Alejandra"
+let texto2 ="Alejandra2"
+let verdadero= true
+let falso= false
+let object= {}
+let array= []
+
+let x= 100
+let y= 50
+console.log ( x + y)
+
+let suma= x+y
+console.log (suma)
+
+let cambiar= 1500
+console.log (cambiar)
+cambiar =10
+console.log (cambiar)
+cambiar ="Texto"
+console.log (cambiar)
+
+
+let frutas= ["melon", "sandia", "fresa"]
+console. log (frutas)
+console.log (frutas[1])
+
+frutas[3]="Manzana"
+console. log (frutas)
+
+frutas.push ("guayaba")
+console. log (frutas)
+
+frutas.pop ()
+
+frutas.splice(1,2)
+
+let persona= {
+    nombre: "Alejandra",
+    direccion: "Azcapo",
+    telefono: "5512456545",
+    edad: "21",
+    trabajos: ["Dev:f", "Developer", "Mami"],
+    mascotas:{
+        perrito: "salchicha",
+        gatito: "siames",
+        pez: "dorado",
+    }
+}
+console.log(persona.mascotas.pez)
---
+++ 04_Javascript/main_challenge.js
@@ -0,0 +1,43 @@
+//let nombre =prompt ("nombre");
+//let edad =prompt ("edad");
+//let telefono =prompt ("telefono");
+
+//console.log ("Nombre: "+ nombre);
+//console.log ("edad: " + ""+edad);
+//console.log ("telefono: " + ""+telefono);
+
+//let fecha = prompt ("ingresa tu año de nacimiento");
+//let anioactual=  2019;
+//console.log (anioactual-fecha);
+
+
+//let fecha = Number (prompt ("ingresa tu año de nacimiento"));
+//let anioactual=  2019;
+//console.log (anioactual-fecha);
+
+
+let persona= {
+    nombre: "Alejandra"
+    direccion: "Azcapo"
+    telefono: "5512456545" 
+    peliculas: [tipos]
+    {
+        accion: "duro de matar", "el gran asalto", "mision imposible"
+        romanticas: "cartas a julieta", "bajo la misma estrella", "cuestion de tiempo"
+        comedia: "sexy por acidente", "johny english", "hombre al agua"
+        musical: "mamma mia", "la la land", "los miserables"
+        terror: "hannibal", "voces del más alla", "el legado del diablo"
+    }
+    musica: [tipos]
+    {
+        salsa: "Hector Lavoe", "Frankie Ruiz", "Oscar D leon"
+        pop: "Belinda", "Kalimba", "ov7"
+        banda: "la arrolladora", "julion alvarez", "luis coronel"
+        rock: "tri", "cafe tacuba", "mana"
+        metal: "x", "marilyn", "ccc"
+    }
+}
+
+
+
+
